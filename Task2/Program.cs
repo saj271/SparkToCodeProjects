155b066907c2ad2d01ab1f123a388afb0be24e36
saja majid <saja27.m@gmail.com>
2026-07-15 11:10:20 +0400

    solve problem1

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Task2
+﻿using System;
+
+namespace Task2Solution
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ////////////////////////////////////////////////////
+            // Task 1 - Countdown Timer
+            ////////////////////////////////////////////////////
+
+            Console.Write("Enter the starting number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = number; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Liftoff!");
         }
     }
 }
