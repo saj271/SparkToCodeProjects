7f022101eae0edb442487f7ca54a0201798f0dd2
saja majid <saja27.m@gmail.com>
2026-07-16 13:54:36 +0400

    Solve problem 1

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Task3
+﻿using System;
+
+namespace Task2Solution
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ////////////////////////////////////////////////////
+            // Task 1 - Absolute Difference
+            ////////////////////////////////////////////////////
+
+            Console.WriteLine("Task 1 - Absolute Difference");
+
+            Console.Write("Enter the first number: ");
+            double firstNumber = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            double difference = firstNumber - secondNumber;
+            double absoluteDifference = Math.Abs(difference);
+
+            Console.WriteLine("Absolute Difference = " + absoluteDifference);
         }
     }
 }
