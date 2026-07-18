ac6f697bbfe64777768de4173bb4ff68cf223183
saja majid <saja27.m@gmail.com>
2026-07-18 23:17:40 +0400

    Solve problem 1

---
+++ Task5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////////////////////////////////////////////////////
+            // Task 1 - Fixed Grades Array
+            ////////////////////////////////////////////////////
+
+            Console.WriteLine("Task 1 - Fixed Grades Array");
+
+            int[] fixedGrades = new int[5];
+
+            for (int i = 0; i < fixedGrades.Length; i++)
+            {
+                Console.Write("Enter grade " + (i + 1) + ": ");
+                fixedGrades[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Entered Grades:");
+
+            foreach (int grade in fixedGrades)
+            {
+                Console.WriteLine(grade);
+            }
+
+           
+        }
+    }
+}
