add99a3b0a8e6aea6799d3b2f67349d112096c5d
saja majid <saja27.m@gmail.com>
2026-07-18 22:12:30 +0400

    Solve problem 6-7-8

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -10,29 +10,31 @@ namespace Task4
             // Task 1 - Personalized Welcome Function
             ////////////////////////////////////////////////////
 
+            Console.WriteLine("Task 1 - Personalized Welcome Function");
+
             Console.Write("Enter your name: ");
             string userName = Console.ReadLine();
 
             PrintWelcome(userName);
 
-            Console.WriteLine();
-
             ////////////////////////////////////////////////////
             // Task 2 - Square Number Function
             ////////////////////////////////////////////////////
 
+            Console.WriteLine("Task 2 - Square Number Function");
+
             Console.Write("Enter a number: ");
-            int number = int.Parse(Console.ReadLine());
+            int squareNumber = int.Parse(Console.ReadLine());
 
-            int result = Square(number);
+            int squareResult = Square(squareNumber);
 
-            Console.WriteLine("Square = " + result);
-
-            Console.WriteLine();
+            Console.WriteLine("Square = " + squareResult);
 
             ////////////////////////////////////////////////////
             // Task 3 - Celsius to Fahrenheit Function
             ////////////////////////////////////////////////////
+
+            Console.WriteLine("Task 3 - Celsius to Fahrenheit Function");
 
             Console.Write("Enter temperature in Celsius: ");
             double celsius = double.Parse(Console.ReadLine());
@@ -41,13 +43,78 @@ namespace Task4
 
             Console.WriteLine("Temperature in Fahrenheit = " + fahrenheit);
 
-            Console.WriteLine();
-
             ////////////////////////////////////////////////////
             // Task 4 - Fixed Menu Display Function
             ////////////////////////////////////////////////////
 
+            Console.WriteLine("Task 4 - Fixed Menu Display Function");
+
             DisplayMenu();
+
+            ////////////////////////////////////////////////////
+            // Task 5 - Even or Odd Function
+            ////////////////////////////////////////////////////
+
+            Console.WriteLine("Task 5 - Even or Odd Function");
+
+            Console.Write("Enter a number: ");
+            int evenNumber = int.Parse(Console.ReadLine());
+
+            bool isNumberEven = IsEven(evenNumber);
+
+            if (isNumberEven)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+
+            ////////////////////////////////////////////////////
+            // Task 6 - Rectangle Area & Perimeter Functions
+            ////////////////////////////////////////////////////
+
+            Console.WriteLine("Task 6 - Rectangle Area & Perimeter Functions");
+
+            Console.Write("Enter length: ");
+            double rectangleLength = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter width: ");
+            double rectangleWidth = double.Parse(Console.ReadLine());
+
+            double rectangleArea =
+                CalculateArea(rectangleLength, rectangleWidth);
+
+            double rectanglePerimeter =
+                CalculatePerimeter(rectangleLength, rectangleWidth);
+
+            Console.WriteLine("Area = " + rectangleArea);
+            Console.WriteLine("Perimeter = " + rectanglePerimeter);
+
+            ////////////////////////////////////////////////////
+            // Task 7 - Grade Letter Function
+            ////////////////////////////////////////////////////
+
+            Console.WriteLine("Task 7 - Grade Letter Function");
+
+            Console.Write("Enter score: ");
+            int gradeScore = int.Parse(Console.ReadLine());
+
+            string gradeLetter = GetGradeLetter(gradeScore);
+
+            Console.WriteLine("Grade = " + gradeLetter);
+
+            ////////////////////////////////////////////////////
+            // Task 8 - Countdown Function
+            ////////////////////////////////////////////////////
+
+            Console.WriteLine("Task 8 - Countdown Function");
+
+            Console.Write("Enter starting number: ");
+            int countdownStart = int.Parse(Console.ReadLine());
+
+            Countdown(countdownStart);
         }
 
         static void PrintWelcome(string name)
@@ -63,39 +130,61 @@ namespace Task4
 
         static double CelsiusToFahrenheit(double celsius)
         {
-            double fahrenheit = (celsius * 9 / 5) + 32;
-            return fahrenheit;
+            return (celsius * 9 / 5) + 32;
         }
 
         static void DisplayMenu()
         {
-            Console.WriteLine("========== MENU ==========");
             Console.WriteLine("1) Start");
             Console.WriteLine("2) Help");
             Console.WriteLine("3) Exit");
-
-            ////////////////////////////////////////////////////
-            // Task 5 - Even or Odd Function
-            ////////////////////////////////////////////////////
-
-            Console.Write("Enter a number: ");
-            int number = int.Parse(Console.ReadLine());
-
-            bool result = IsEven(number);
-
-            if (result)
-            {
-                Console.WriteLine("Even");
-            }
-            else
-            {
-                Console.WriteLine("Odd");
-            }
         }
 
         static bool IsEven(int number)
         {
             return number % 2 == 0;
+        }
+
+        static double CalculateArea(double length, double width)
+        {
+            return length * width;
+        }
+
+        static double CalculatePerimeter(double length, double width)
+        {
+            return 2 * (length + width);
+        }
+
+        static string GetGradeLetter(int score)
+        {
+            if (score >= 90)
+            {
+                return "A";
+            }
+            else if (score >= 80)
+            {
+                return "B";
+            }
+            else if (score >= 70)
+            {
+                return "C";
+            }
+            else if (score >= 60)
+            {
+                return "D";
+            }
+            else
+            {
+                return "F";
+            }
+        }
+
+        static void Countdown(int startingNumber)
+        {
+            for (int i = startingNumber; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
